b43c7220fbb42a1d11761800a2c5e17186ae9c49
cumber96 <nowgnoheel@gmail.com>
2026-07-28 08:46:59 +0900

    feat: add Apple Watch target

---
+++ RunContent/RunContent Watch App Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RunContent Watch App Watch App
+//
+//  Created by 이홍원 on 7/28/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RunContent/RunContent Watch App Watch App/RunContent_Watch_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunContent_Watch_AppApp.swift
+//  RunContent Watch App Watch App
+//
+//  Created by 이홍원 on 7/28/26.
+//
+
+import SwiftUI
+
+@main
+struct RunContent_Watch_App_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
