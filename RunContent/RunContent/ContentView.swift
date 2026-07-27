801f9fc3a7bf13c9bfc1b61ccaa2cec8664e543e
Hongwon Lee <nowgnoheel@gmail.com>
2026-07-27 23:22:20 +0900

    chore: initialize iOS project

---
+++ RunContent/RunContent/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RunContent
+//
+//  Created by 이홍원 on 7/27/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RunContent/RunContent/RunContentApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunContentApp.swift
+//  RunContent
+//
+//  Created by 이홍원 on 7/27/26.
+//
+
+import SwiftUI
+
+@main
+struct RunContentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
